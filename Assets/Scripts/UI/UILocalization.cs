5e2811bf5a381136339ac4895c01e40328ddac0a
JasonMadness <tobc_madness@mail.ru>
2023-05-29 16:10:20 +0300

    temp commit

--- Assets/Scripts/UI/UILocalization.cs
+++ Assets/Scripts/UI/UILocalization.cs
@@ -1,12 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Localization;
 using UnityEngine.Localization.Settings;
+using UnityEngine.Localization.Tables;
 
 public class UILocalization : MonoBehaviour
 {
+    [SerializeField] LocalizedStringTable _stringTable;
+    public LocalizedString[] locStr;
+    
+    private void Start()
+    {
+        GetWaveLocalizedName();
+    }
+    
+
     public string GetWaveLocalizedName()
     {
-        return "";
+        StringTableEntry entry = null;
+        StringTable table = _stringTable.GetTable();
+        SharedTableData sharedData = table.SharedData;
+        var x = sharedData.Entries[0];
+        table.TryGetValue(0, out entry);
+        Debug.Log(entry.GetLocalizedString());
+        return entry.GetLocalizedString();
     }
 }
