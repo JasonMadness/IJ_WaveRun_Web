3481f70705ac324beffdeb974b94fe74b7c881e3
JasonMadness <tobc_madness@mail.ru>
2023-10-31 06:28:11 +0300

    temp

--- Assets/Scripts/Misc/Yandex/Leaderboard.cs
+++ Assets/Scripts/Misc/Yandex/Leaderboard.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using Agava.YandexGames;
 using UnityEngine;
 
@@ -5,6 +6,38 @@ public class Leaderboard : MonoBehaviour
 {  
     private const string ANONYMOUS_NAME = "Anonymous";
     private const string LEADERBOARD_NAME = "WaveRunLeaderboard";
+
+    [SerializeField] private LeaderboardView _leaderboardView;
+
+    private readonly List<LeaderboardPlayer> _leaderboardPlayers = new();
+
+    public void SetPlayer(int score)
+    {
+        if (PlayerAccount.IsAuthorized == false)
+        {
+            return;
+        }
+
+        Agava.YandexGames.Leaderboard.GetPlayerEntry(LEADERBOARD_NAME, _ => 
+        {
+            Agava.YandexGames.Leaderboard.SetScore(LEADERBOARD_NAME, score);
+        });
+    }
+
+    private void Fill()
+    {
+        _leaderboardPlayers.Clear();
+
+        if (PlayerAccount.IsAuthorized == false)
+        {
+            return;
+        }
+
+        Agava.YandexGames.Leaderboard.GetEntries(LEADERBOARD_NAME, onSuccessCallback: =>
+        {
+
+        })
+    }
 
     public void OpenLeaderboard()
     {
